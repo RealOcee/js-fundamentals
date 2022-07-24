5ee66b1d7a4e8cafd3ee7453b56925c8e19da26f
Real Ocee <onasosi@gmail.com>
2022-07-24 23:49:26 +0100

    numbers

--- app.js
+++ app.js
@@ -1,9 +1,73 @@
+// const number = 34;
+// // let decimal = 2.466;
+// // decimal = 'are great!'
+// const number2 = 2.467;
+// const number3 = 2.468;
 
-const name = 'Andrew';
-const LastName = 'Onaulogho';
+// const add = number + number2
+// const sub = number - number2
+// const mult = number * number2
+// const div = number / number2
 
-let fullName =+ name + ' ' + LastName;
-console.log('Hello there my name is' );
+
+// // console.log(number);
+// // console.log(sub);
+// // console.log(mult);
+// // console.log(div);
+// console.log(number3);
+
+//  Numbers
+// +=, -=, /=, *=, ++, --, %
+// Modulus (%) operator returns the remainder after integer division
+
+let number = 40;
+number += 5;
+number += 5;
+number += 5;
+number += 5;
+number += 5;
+number += 5;
+number += 5;
+number -= 5;
+number ++;
+number--;
+number--;
+
+const slices = 10;
+const children = 3;
+const amount = slices / ;
+
+console.log(amount);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
