1e194f39a7fecc081547c5494e42925e8ee31471
Real Ocee <onasosi@gmail.com>
2022-07-30 21:14:12 +0100

    loop

--- app.js
+++ app.js
@@ -217,6 +217,19 @@ if (dice < 1 || dice > 6) {
 
 
 
+// Loops
+// repeatedly run a block of code while condition is true
+// for loop
+
+let i;
+for (i = 0; i < 10; i++) {
+    console.log('and the number is : ' + i);
+}
+
+
+
+
+
 
 
 
