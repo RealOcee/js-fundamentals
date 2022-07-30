a1ee30479508e0c61559fabdcb317867bcb189b2
Real Ocee <onasosi@gmail.com>
2022-07-30 16:24:07 +0100

    dice

--- app.js
+++ app.js
@@ -98,17 +98,332 @@
 // return
 // default, undefined, shortcuts, ignores after
 
-const wallHeight = 80;
+// const wallHeight = 80;
 
-function calculate(value) {
-    // const newValue = value * 2.54;
-    return value * 2.54;
+// function calculate(value) {
+//     // const newValue = value * 2.54;
+//     return value * 2.54;
+// }
+// const width = calculate(100);
+// const height = calculate(wallHeight);
+
+// const dimensions = [width, height];
+// console.log(dimensions);
+
+
+// Function definition/declaration
+// function addValues(num1, num2) {
+//     return num1 + num2;
+// }
+
+// const firstValue = addValues(3,4);
+// const secondValue = addValues(12, 34);
+
+// // Function expression
+
+// const add = function (num1, num2) {
+//     return num1 + num2;
+// }
+
+// // const thirdValue = add(5, 6);
+// const values = [firstValue, secondValue, add(5, 6)];
+// console.log(values);
+
+// Objects - key/value pairs methods
+// Dot notation
+
+// const person = {
+//     name: 'Andrew',
+//     lastName: 'John',
+//     age: 34,
+//     education: false,
+//     married: true,
+//     siblings: ['anna', 'susan', 'peter'],
+//     greeting: function () {
+//         console.log('Hello my name is ANDREW');
+//     }, 
+// };
+
+// const age = person.age;
+// console.log(age);
+
+
+// console.log(person.name);
+// console.log(person.siblings[2]);
+// person.greeting();
+
+
+// Conditional Statements
+// comparison Operators 
+// >, <, >=, <=, ===, !=, !===
+// else if and !
+
+// const num1 = 6;
+// const num2 = 6;
+
+// const result = num1 >= num2;
+
+
+// if (num1 > num2) {
+//     console.log('first number is bigger than second');
+// } else if (result) {
+//     console.log('first number equal to a second');
+// } else {
+//     console.log('second number is bigger than first');
+// }
+
+// == Checks only value
+// === Checks value and type
+
+// const num1 = 6;
+// const num2 = '6';
+
+// const value = num1 != num2
+// const value2 = num1 !== num2;
+
+// console.log(value);
+// console.log(value2);
+
+
+
+// Logical Operators
+// (|| - OR), (&& - AND), !
+
+// const name = 'bob';
+// const age = 24;
+
+// if (name === 'bob') {
+//     console.log('hello there user');
+// } else {
+//     console.log('wrong values');
+// }
+
+
+
+// Switch 
+// dice values : 1 - 6
+
+const dice = 0;
+
+if (dice === 1) {
+    console.log('you got one');
 }
-const width = calculate(100);
-const height = calculate(wallHeight);
+if (dice === 2) {
+    console.log('you got two');
+}
+if (dice < 1 || dice > 6) {
+    log
+}
 
-const dimensions = [width, height];
-console.log(dimensions);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
