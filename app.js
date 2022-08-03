e50b87a4b310135ac472a66901a1c75d3e7044a3
Real Ocee <onasosi@gmail.com>
2022-08-03 16:29:57 +0100

    for loop

--- app.js
+++ app.js
@@ -203,30 +203,52 @@
 // Switch 
 // dice values : 1 - 6
 
-const dice = 0;
+// const dice = 0;
 
-if (dice === 1) {
-    console.log('you got one');
-}
-if (dice === 2) {
-    console.log('you got two');
-}
-if (dice < 1 || dice > 6) {
-    log
-}
+// if (dice === 1) {
+//     console.log('you got one');
+// }
+// if (dice === 2) {
+//     console.log('you got two');
+// }
+// if (dice < 1 || dice > 6) {
+//     log
+// }
 
 
 
 // Loops
 // repeatedly run a block of code while condition is true
+// while loop
+
+
+// let amount = 10;
+//  while(amount > 0){
+//      console.log('I have ' + amount + " dollars and I'm going to the mall");
+//      amount--;
+//  }
+
+
+// Loops
+// repeatedly run a block of code while condition is true
+// do while loop
+// code block first, condition second
+// runs at least
+
+// let money = 0;
+
+// do{
+//     console.log('You have ' + money + ' dollars');
+//     money++;
+// }while (money < 10);
+
+// Loops
+// repeatedly run a block of code while condition is true
 // for loop
 
-let i;
-for (i = 0; i < 10; i++) {
-    console.log('and the number is : ' + i);
+for (let number = 11; number >= 0; number--) {
+    console.log('and the number is : ' + number);
 }
-
-
 
 
 
