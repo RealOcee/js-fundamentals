7f4c6ead621afa2fbe7f7e3466276521c92e226e
Real Ocee <onasosi@gmail.com>
2022-08-04 22:59:36 +0100

    Interpolation

--- connect the dot/app.js
+++ connect the dot/app.js
@@ -1,21 +1,41 @@
 // String properties and methods
-// Wrapper String Object, don't memorize methods
-let text = ' Andrew Onaulogho';
-let result = text.length;
-console.log(result);
+// Wrapper String Object.
 
-console.log(text.length);
-console.log(text.toLowerCase());
-console.log(text.toUpperCase());
-console.log(text.charAt(0));
-console.log(text.charAt(text.length - 1));
-console.log(text.indexOf('o'));
-console.log(text);
-console.log(text.trim());
-console.log(text.trim().toLowerCase().startsWith('andrew'));
-console.log(text.includes('rew'));
-console.log(text.slice(0, 2));
-console.log(text.slice(-5));
+// let text = ' Andrew Onaulogho';
+// let result = text.length;
+// console.log(result);
+
+// console.log(text.length);
+// console.log(text.toLowerCase());
+// console.log(text.toUpperCase());
+// console.log(text.charAt(0));
+// console.log(text.charAt(text.length - 1));
+// console.log(text.indexOf('o'));
+// console.log(text);
+// console.log(text.trim());
+// console.log(text.trim().toLowerCase().startsWith('andrew'));
+// console.log(text.includes('rew'));
+// console.log(text.slice(0, 2));
+// console.log(text.slice(-5));
+
+
+
+// Template Literals - ES6+
+// Backtick characters '' - above tab (left from one)
+// Interpolation ${} - insert expression(value)
+
+// const name = 'andrew';
+// const age = 25;
+// const sentence = "Hey it's " + name + ' and he is ' + age + ' years old';
+// console.log(sentence);
+
+// const value = `Hey it's ${name} and he is ${age}`;
+// console.log(value);
+
+
+
+
+
 
 
 
