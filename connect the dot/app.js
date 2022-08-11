c207797ea1adac7c49da744e6e5a4c665b291743
Real Ocee <onasosi@gmail.com>
2022-08-11 17:06:48 +0100

    Variable Lookup & code block

--- connect the dot/app.js
+++ connect the dot/app.js
@@ -273,3 +273,21 @@
 // console.log(`my name is ${name} and I'm awesome`);
 
 
+// Variable Lookup
+// {} - code block
+
+const globalNumber = 5;
+
+function add(num1, num2) {
+    // const globalNumber = 20;
+    const result = num1 + num2 + globalNumber;
+    function multiply() {
+        // const globalNumber = 100;
+        const multiplyResult = result * globalNumber;
+        console.log(multiplyResult); 
+    }
+    multiply();
+    return result;
+}
+
+console.log(add(3, 4));
