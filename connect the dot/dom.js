07514c6aa95cf1ae08074d7820d69ae595dd3cd7
Real Ocee <onasosi@gmail.com>
2022-08-19 23:56:36 +0100

    added

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -1,31 +1,68 @@
-// similar to css
+// // similar to css
 
-// select the element or group of elements the you we want to affect
-// decide the effect we want to apply to the selection
+// // select the element or group of elements the you we want to affect
+// // decide the effect we want to apply to the selection
 
-// many different ways
+// // many different ways
 
-document.body.style.backgroundColor = 'blue';
-document.body.style.color = 'yellow';
-document.getElementById('btn').style.color = 'red';
+// document.body.style.backgroundColor = 'blue';
+// document.body.style.color = 'yellow';
+// document.getElementById('btn').style.color = 'red';
 
-// assign to a variable
+// // assign to a variable
 
-const element = document.getElementById('element');
+// const element = document.getElementById('element');
 
-// do something
+// // do something
 
-document.querySelector('element'); // do something;
+// document.querySelector('element'); // do something;
 
-// // window object
-// console.log(window);
+// // // window object
+// // console.log(window);
 
-// returns a node object or a node list, which is an arraylike object
+// // returns a node object or a node list, which is an arraylike object
 
-const btn = document.getElementById('btn');
-const name = btn.nodeName;
-const css = btn.style;
-console.log(btn);
+// const btn = document.getElementById('btn');
+// const name = btn.nodeName;
+// const css = btn.style;
+// console.log(btn);
 
-// console.log(name);
-// console.log(css);
+// // console.log(name);
+// // console.log(css);
+
+
+// window object = browser api
+// document 
+// console.dir
+
+
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+
+// getElementById('element')
+
+// const h1 = document.getElementById('title');
+// h1.style.color = 'red';
+
+// const btn = document.getElementById('btn');
+
+// btn.style.backgroundColor = 'blue';
+// btn.style.color = 'white';
+
+
+
+
+// Select the element or group of elements the we want
+// Decide the effect we want to apply to the selection
+
+// getElementsByTagName('tagname');
+// node-list = array-like object
+// index, length property but not array methods
+
+const headings = document.getElementsByTagName('h2');
+headings[0].style.color = 'red';
+console.log(headings.length);
+
+
+
