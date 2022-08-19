cf4b379ae138be15ff75bbd36efa6b8aac372bb9
Real Ocee <onasosi@gmail.com>
2022-08-19 06:27:26 +0100

    initial commit

--- connect the dot/app.js
+++ connect the dot/app.js
@@ -502,18 +502,21 @@ const days = [
 ]
 
 // const date = new Date();
-const date = new Date('1/12/2004')
-const month = date.getMonth();
-console.log(months, [month]);
+// const date = new Date('1/12/2004')
+// const month = date.getMonth();
+// console.log(months, [month]);
 
-const day = date.getDay();
-console.log(days [day]);
+// const day = date.getDay();
+// console.log(days [day]);
 
-console.log(date.getDate());
-console.log(date.getFullYear());
+// console.log(date.getDate());
+// console.log(date.getFullYear());
 
-const sentence = `${days[day]}, ${date.getDate()} ${
-    months[month]
-} ${date.getFullYear()}`;
+// const sentence = `${days[day]}, ${date.getDate()} ${
+//     months[month]
+// } ${date.getFullYear()}`;
 
-document.body.innerHTML = sentence 
+// document.body.innerHTML = sentence 
+
+
+// END HAS COME! //
