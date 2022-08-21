1a4dccec6bf93b44a9aec1c2635b5345a3b11d2b
Real Ocee <onasosi@gmail.com>
2022-08-21 18:47:47 +0100

    update

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -136,3 +136,27 @@
 // nextSibling
 // return whitespace
 
+// const first = document.querySelector('.first');
+// const second = (first.nextSibling.nextSibling.style.color = 'red');
+// console.log(second);
+
+// const last = document.querySelector('#last');
+// const third = last.previousSibling.previousSibling;
+// console.log(third);
+// console.log(last.nextSibling.nextSibling);
+
+
+
+
+
+// previousElementSibling
+// nextElementSibling
+
+// const first = document.querySelector('.first');
+// first.nextElementSibling.style.color = 'red';
+// const last = document.querySelector('#last');
+
+
+
+
+
