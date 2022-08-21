46222db94d784d15a6f722f413b29fe7d201b5c1
Real Ocee <onasosi@gmail.com>
2022-08-21 18:58:57 +0100

    update

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -158,5 +158,21 @@
 
 
 
+// nodeValue
+// textContent
+
+
+// const item = document.getElementById('special');
+// const value = item.firstChild.nodeValue;
+// const easyValue = item.textContent;
+
+// console.log(easyValue);
+
+
+
+
+// getAttribute();
+// setAtrribute();
+
 
 
