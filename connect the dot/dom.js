e7620f1a702681aa0a79fb05f6beb5543e614bdd
Real Ocee <onasosi@gmail.com>
2022-08-21 20:39:37 +0100

    update

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -172,7 +172,23 @@
 
 
 // getAttribute();
-// setAtrribute();
+// setAttribute();
+
+const first = document.querySelector('.first');
+const classValue = first.getAttribute('class');
+// console.log(classValue);
+
+const link = document.getElementById('link');
+const showLink = link.getAttribute('href');
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute('class', 'first');
+last.textContent = 'I also have a class of first';
+console.log(last);
+
+const links = document.querySelectorAll('.first');
+console.log(links);
 
 
 
