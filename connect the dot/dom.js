69ec372362f3d0e866174fa6423cc48d26244771
Real Ocee <onasosi@gmail.com>
2022-08-22 06:06:48 +0100

    update

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -229,23 +229,54 @@
 // createTextNode('text content')
 // element.appendChild(childElement)
 
-const result = document.querySelector('#result');
+// const result = document.querySelector('#result');
 
-// create empty element
+// // create empty element
+
+// const bodyDiv = document.createElement('div');
+
+// // create text node
+
+// const text = document.createTextNode('a simple body div');
+// bodyDiv.appendChild(text);
+// document.body.appendChild(bodyDiv);
+
+// const heading = document.createElement('h2');
+// const headingText = document.createTextNode('dynamic heading');
+// heading.appendChild(headingText);
+// heading.classList.add('blue');
+
+// result.appendChild(heading);
+
+// console.log(result.children);
+
+
+
+
+// createTextNode('text content')
+// element.appendChild(childElement)
+
+
+// insertBefore('element', 'location');
+
+ const result = document.querySelector('#result');
+
+// // create empty element
 
 const bodyDiv = document.createElement('div');
 
-// create text node
+// // create text node
 
 const text = document.createTextNode('a simple body div');
 bodyDiv.appendChild(text);
-document.body.appendChild(bodyDiv);
+
 
 const heading = document.createElement('h2');
 const headingText = document.createTextNode('dynamic heading');
 heading.appendChild(headingText);
 heading.classList.add('blue');
 
-result.appendChild(heading);
+
 
 console.log(result.children);
+
