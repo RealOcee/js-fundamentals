6e6c2a0203a3e88f7d31f86f8376bdf13ef574e4
Real Ocee <onasosi@gmail.com>
2022-08-23 11:42:02 +0100

    added

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -312,16 +312,18 @@
 // how to get a value
 
 
-const form = document.getElementById('form');
-const name = document.getElementById('name');
-const password = document.getElementById('password');
+// const form = document.getElementById('form');
+// const name = document.getElementById('name');
+// const password = document.getElementById('password');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    console.log('form submitted');
-    console.log(name.value);
-    console.log(password.value);
-});
+// form.addEventListener('submit', function(e) {
+//     e.preventDefault();
+//     console.log('form submitted');
+//     console.log(name.value);
+//     console.log(password.value);
+// });
+
+
 
 
 
