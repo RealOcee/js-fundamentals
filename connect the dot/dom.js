2157a8f24b82a05c29498ed676644b42a1b93fce
Real Ocee <onasosi@gmail.com>
2022-08-23 09:58:13 +0100

    added

--- connect the dot/dom.js
+++ connect the dot/dom.js
@@ -284,11 +284,22 @@
 
 
 
-// prepend
-// innerText
+// // prepend
+// // innerText
 
 
-const heading = document.createElement('h2');
-heading.innerText = `i am a dynamic heading`;
-document.body.prepend(heading);
+// const heading = document.createElement('h2');
+// heading.innerText = `i am a dynamic heading`;
+// document.body.prepend(heading);
 
+
+
+
+// remove
+// removeChild
+
+const result = document.querySelector('#result');
+// result.remove();
+const heading = result.querySelector('h1');
+result.removeChild(heading);
+console.log(heading);
